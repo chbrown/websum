80de6346e75278a6acb0470d523cee98b222959d
Christopher Brown <io@henrian.com>
2018-04-09 12:44:35 -0500

    Delint via tslint (mostly autofixes)

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {Parser, Handler} from 'htmlparser2';
+import {Parser, Handler} from 'htmlparser2'
 
 export const tagScores = {
   title: 100000,
@@ -16,17 +16,17 @@ export const tagScores = {
   ol: 200,
   script: -100000,
   style: -100000,
-};
+}
 
 export const metaScores = {
   description: 10000,
   keywords: 5000,
   author: 1000,
-};
+}
 
 export interface Snippet {
-  score: number;
-  content: string;
+  score: number
+  content: string
 }
 
 /** From the docs: https://github.com/fb55/htmlparser2/wiki/Parser-options
@@ -46,48 +46,47 @@ export interface Snippet {
     onend()
 */
 export class SummarizingHandler implements Handler {
-  snippets: Snippet[] = [];
+  snippets: Snippet[] = []
   // current score / position
-  private score = 1;
-  private position = -50;
+  private score = 1
+  private position = -50
   constructor(public callback: (error: Error, snippets?: Snippet[]) => void) { }
   onreset() {
-    this.score = 1;
-    this.position = -50;
-    this.snippets = [];
+    this.score = 1
+    this.position = -50
+    this.snippets = []
   }
   onend() {
-    this.callback(null, this.snippets);
+    this.callback(null, this.snippets)
   }
-  onerror(error:Error) {
-    this.callback(error);
+  onerror(error: Error) {
+    this.callback(error)
   }
-  onopentag(name: string, attribs: {[index:string]: string}) {
-    this.score += tagScores[name] || 0;
+  onopentag(name: string, attribs: {[index: string]: string}) {
+    this.score += tagScores[name] || 0
 
-    if (name == 'meta' && attribs['name'] && attribs['content']) {
+    if (name == 'meta' && attribs.name && attribs.content) {
       this.snippets.push({
-        score: metaScores[attribs['name'].toLowerCase()],
-        content: attribs['content'],
-      });
+        score: metaScores[attribs.name.toLowerCase()],
+        content: attribs.content,
+      })
     }
   }
   onclosetag(name) {
-    this.score -= tagScores[name] || 0;
+    this.score -= tagScores[name] || 0
   }
   ontext(data) {
+    let content = data
     if (this.snippets.length && this.snippets[this.snippets.length - 1].score == this.score) {
       // merge if possible
-      var last_span = this.snippets.pop();
-      data = last_span.content + data;
+      const last_span = this.snippets.pop()
+      content = last_span.content + content
     }
-    this.position++;
+    this.position++
 
-    this.snippets.push({
-      score: this.score - this.position,
-      content: data,
-    });
-  };
+    const score = this.score - this.position
+    this.snippets.push({score, content})
+  }
 }
 
 export function joinSnippets(snippets: Snippet[], minimumScore: number): string {
@@ -101,19 +100,19 @@ export function joinSnippets(snippets: Snippet[], minimumScore: number): string 
     // clean up whitespace
     .map(({content}) => content.replace(/\s+/g, ' ').trim())
     // join into single string
-    .join('; ');
+    .join('; ')
 }
 
 export function summarizeHtml(inputHtml: string,
-                                      minimumScore: number,
-                                      callback: (error: Error, result?: string) => void) {
-  let handler = new SummarizingHandler((error, snippets) => {
-    if (error) return callback(error);
-    let result = joinSnippets(snippets, minimumScore);
-    callback(null, result);
-  });
+                              minimumScore: number,
+                              callback: (error: Error, result?: string) => void) {
+  const handler = new SummarizingHandler((error, snippets) => {
+    if (error) return callback(error)
+    const result = joinSnippets(snippets, minimumScore)
+    callback(null, result)
+  })
 
-  let parser = new Parser(handler, {decodeEntities: true});
-  parser.write(inputHtml);
-  parser.end();
+  const parser = new Parser(handler, {decodeEntities: true})
+  parser.write(inputHtml)
+  parser.end()
 }
